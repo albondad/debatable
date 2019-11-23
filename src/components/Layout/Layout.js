cfbe8e43a77075cb4e6bdbd9c1f17ee331d0bef8
albondad <36680782+albondad@users.noreply.github.com>
2019-11-23 00:16:45 -0600

    Add files via upload

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Body from '../Body/Body'
+
+class Layout extends Component {
+  state = {
+  }
+
+  render() {
+    <div></div>
+  }
+}
+
+export default Layout;
