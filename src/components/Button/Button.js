fcb3357a4dabadbdcdcf3c5f836ad0bb4494963f
Rishabh Mehta <44340780+Rish7474@users.noreply.github.com>
2019-11-23 02:31:45 -0600

    Add files via upload

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Button extends Component {
+  state = {
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Button;
