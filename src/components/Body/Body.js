28367105fe77f8c842b520a544ba1aa4650a9764
SmitPurohit <35670650+SmitPurohit@users.noreply.github.com>
2019-11-23 03:33:00 -0600

    Add files via upload

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import TopicSelections from "../TopicSelections/TopicSelections"
-import RoomSelection from "../RoomSelection/RoomSelection"
-const body = () => {
+import TopicSelections from "../topicSelections/topicSelections"
+import RoomSelection from "../RoomSelections/RoomSelection"
+import Link from "../Link/Link"
+import Home from "../Home/Home"
+import Topics from "../Topics/Topics"
+const body = (props) => {
   return (
+
     <div>
-    
     </div>
+
+
   )
 }
 
